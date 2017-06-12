0d9529fe8914aa29407459e1f8783762c9fa5751
Amaury Ortega <amauryocortega@gmail.com>
2017-06-12 13:21:26 -0500

    Codigo funcional
    Se pede añadir una interfaz grafica donde se seleccione el archivo
    a escanear

--- App/src/app/Main.java
+++ App/src/app/Main.java
@@ -1,10 +1,17 @@
 package app;
+
 import com.asprise.ocr.Ocr;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ocr.setUp();
+        Ocr ocr = new Ocr();
+        ocr.startEngine("spa", Ocr.SPEED_FASTEST);
+        String texto = ocr.recognize(new File[]{new File("test-image.png")}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+        System.out.println("Result: " + texto);
+        ocr.stopEngine();
     }
-    
+
 }
